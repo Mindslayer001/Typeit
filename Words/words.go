070212878c1babef8345bc5a89d5b5e23c41a5af
MindSlayer001 <manisankarchintagunti@gmail.com>
2025-08-06 23:18:50 +0530

    Moved Words.go to Words folder

---
+++ Words/words.go
@@ -0,0 +1,19 @@
+package words
+
+// FIXME: Add More words to the list
+func GetWords() []string {
+	return []string{
+		"hello",
+		"world",
+		"go",
+		"programming",
+		"language",
+		"example",
+	}
+}
+
+func GetString() string {
+	return string("hello" + "world" + "go" + "programming" + "language" + "example")
+}
+
+// TODO: Add functionality to read random words from the list
